8aa563ecbaec0e3b487c10e5528383f9c3042a61
dgruending <dgruending23@gmail.com>
2021-10-25 10:10:05 +0000

    Completed stage 1

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,2 +1,8 @@
-# write your code here
+from statistics import mean
 
+
+if __name__ == '__main__':
+    # stage 1
+    scores = [int(input()) for _ in range(3)]
+    print(mean(scores))
+    print("Congratulations, you are accepted!")
