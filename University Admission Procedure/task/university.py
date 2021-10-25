7f8ad62f86b231fb374d908444cc45c2cf18517e
dgruending <dgruending23@gmail.com>
2021-10-25 10:15:51 +0000

    Completed stage 2

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -3,6 +3,15 @@ from statistics import mean
 
 if __name__ == '__main__':
     # stage 1
+    # scores = [int(input()) for _ in range(3)]
+    # print(mean(scores))
+    # print("Congratulations, you are accepted!")
+
+    # stage 2
     scores = [int(input()) for _ in range(3)]
-    print(mean(scores))
-    print("Congratulations, you are accepted!")
+    mean_score = mean(scores)
+    print(mean_score)
+    if mean_score >= 60.0:
+        print("Congratulations, you are accepted!")
+    else:
+        print("We regret to inform you that we will not be able to offer you admission.")
